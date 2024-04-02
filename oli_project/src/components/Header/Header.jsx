a7b359df160103d48003c1999ea03ef505530b37
Henry Felipe Bense de Oliveira <henry.oliveira11@etec.sp.gov.br>
2024-04-02 17:22:23 -0300

    Ajustes no header da home e adicionado seção de traile

--- oli_project/src/components/Header/Header.jsx
+++ oli_project/src/components/Header/Header.jsx
@@ -10,39 +10,62 @@ import styles from './styles.module.css'
 import React from 'react'
 
 const Header = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navContainer}>
+        <NavLink className={styles.logoImage} to='/'>
+          <img src={logo} alt='logo' width={'100rem'} />
+        </NavLink>
+
+        <ul className={styles.navLinks}>
+          <li>
+            <NavLink to='/'>
+              Home
+            </NavLink>
+            <NavLink to='/loja'>
+              Loja
+            </NavLink>
+            <NavLink to='/wiki'>
+              Wiki
+            </NavLink>
+            <NavLink to='/mapa'>
+              Mapa
+            </NavLink>
+            <NavLink to='/regras'>
+              Regras
+            </NavLink>
+            <NavLink to='/contact'>
+              Contato
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+
+  function Switch() {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const toggleChecked = () => {
+      setIsSelected((prev) => !prev);
+    };
+
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.navContainer}>
-                <NavLink className={styles.logoImage} to='/'>
-                    <img src={logo} alt='logo' width={'100rem'} />
-                </NavLink>
-
-                <ul className={styles.navLinks}>
-                    <li>
-                        <NavLink to='/'>
-                            Home
-                        </NavLink>
-                        <NavLink to='/loja'>
-                            Loja
-                        </NavLink>
-                        <NavLink to='/wiki'>
-                            Wiki
-                        </NavLink>
-                        <NavLink to='/mapa'>
-                            Mapa
-                        </NavLink>
-                        <NavLink to='/regras'>
-                            Regras
-                        </NavLink>
-                        <NavLink to='/contact'>
-                            Contato
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
-
+      <div className={styles.switchContainer}>
+        <div
+          className={classnames(styles.switchDefault, {
+            [styles.switchSelected]: isSelected,
+          })}
+          onClick={toggleChecked}
+        >
+          <span className={styles.switchBall}></span>
+        </div>{' '}
+        <div className={styles.icon}>
+          {isSelected ? <img src={darkIcon} /> : <img src={lightIcon} />}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- oli_project/src/pages/Home/Home.jsx
+++ oli_project/src/pages/Home/Home.jsx
@@ -1,10 +1,71 @@
 //css
 import styles from './styles.module.css'
 
-export default function Home(){
-    return(
-    <div className={styles.container}>
-        <h1>pagina home</h1>
-    </div>
+//logo
+import Logo from '../../assets/logoServer.png'
+
+//videos
+import trailerPrincipal from '../../assets/minecraftTrailer.mp4'
+
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function Home() {
+    //Da play no trailer quando chegar em pelo menos 40% da section
+    const [isVideoReady, setIsVideoReady] = useState(false);
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.4 
+        };
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting && isVideoReady) {
+                    videoRef.current.play();
+                } else {
+                    videoRef.current.pause();
+                }
+            });
+        }, options);
+
+        observer.observe(document.getElementById('trailerSection'));
+
+        return () => {
+            observer.disconnect();
+        };
+    }, [isVideoReady]);
+
+    return (
+        <div className='home'>
+            <section className={styles.headerSection} id='headerSection'>
+                <div className={styles.headerContainer}>
+                    <div className={styles.logoContainer}>
+                        <img src={Logo} alt='logo' width={'250rem'} />
+                    </div>
+                    <h1>Explore um mundo fragmentado da sua realidade com diversos desafios, missões e uma super aventura!</h1>
+                    <div className={styles.btn}><a href="">Conheça o Oli Verse!</a></div>
+                </div>
+            </section>
+
+            <section className={styles.trailerSection} id='trailerSection'>
+            <div className={styles.trailerContainer}>
+                    <div className={styles.trailerVideo}>
+                        <video src={trailerPrincipal} onLoadedData={() => setIsVideoReady(true)} ref={videoRef} controls loop>
+                        </video>
+                    </div>
+
+                    <div className={styles.trailerDescription}>
+                        <p>
+                           orem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla dui in dignissim commodo.
+                           Sed cursus, neque quis cursus pulvinar, libero metus porttitor augue, ac luctus dolor tellus
+                           sed sem. 
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
