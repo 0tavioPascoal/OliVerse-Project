f5942604281a3fa083dbe6ce789a82decd926eed
Henry Felipe Bense de Oliveira <henry.oliveira11@etec.sp.gov.br>
2024-04-02 17:46:48 -0300

    Ajustes anteriores da branch develip-henry
    Adicionado section trailer e ajuste na tela home

--- oli_project/src/pages/Home/Home.jsx
+++ oli_project/src/pages/Home/Home.jsx
@@ -4,19 +4,68 @@ import styles from './styles.module.css'
 //logo
 import Logo from '../../assets/logoServer.png'
 
-export default function Home(){
-    return(
-        <section className={styles.container}>
-        <div className={styles.logo}>
-            <img src={Logo} alt='logo' width={'300rem'}/>
-        </div>
+//videos
+import trailerPrincipal from '../../assets/minecraftTrailer.mp4'
 
-        <div className={styles.text1}>
-            <h1>Explore um mundo fragmentado de sua realidade com</h1>
-            <h1>Diversos desafios, missões e uma super aventura</h1>
+import React, { useEffect, useRef, useState } from 'react';
 
-                <button>Conheça o Oli Verse!</button>
+export default function Home() {
+    //Da play no trailer quando chegar em pelo menos 40% da section
+    const [isVideoReady, setIsVideoReady] = useState(false);
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.4 
+        };
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting && isVideoReady) {
+                    videoRef.current.play();
+                } else {
+                    videoRef.current.pause();
+                }
+            });
+        }, options);
+
+        observer.observe(document.getElementById('trailerSection'));
+
+        return () => {
+            observer.disconnect();
+        };
+    }, [isVideoReady]);
+
+    return (
+        <div className='home'>
+            <section className={styles.headerSection} id='headerSection'>
+                <div className={styles.headerContainer}>
+                    <div className={styles.logoContainer}>
+                        <img src={Logo} alt='logo' width={'250rem'} />
+                    </div>
+                    <h1>Explore um mundo fragmentado da sua realidade com diversos desafios, missões e uma super aventura!</h1>
+                    <div className={styles.btn}><a href="">Conheça o Oli Verse!</a></div>
+                </div>
+            </section>
+
+            <section className={styles.trailerSection} id='trailerSection'>
+            <div className={styles.trailerContainer}>
+                    <div className={styles.trailerVideo}>
+                        <video src={trailerPrincipal} onLoadedData={() => setIsVideoReady(true)} ref={videoRef} controls loop>
+                        </video>
+                    </div>
+
+                    <div className={styles.trailerDescription}>
+                        <p>
+                           orem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla dui in dignissim commodo.
+                           Sed cursus, neque quis cursus pulvinar, libero metus porttitor augue, ac luctus dolor tellus
+                           sed sem. 
+                        </p>
+                    </div>
+                </div>
+            </section>
         </div>
-    </section>
     )
 }
