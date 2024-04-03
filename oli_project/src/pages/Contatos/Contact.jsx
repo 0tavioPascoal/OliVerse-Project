99a374bed527fa3ff5ddd64d8c8057a31d841e04
Henry Felipe Bense de Oliveira <henry.oliveira11@etec.sp.gov.br>
2024-04-03 13:43:05 -0300

    Ajustes na tela contato

--- oli_project/src/pages/Contatos/Contact.jsx
+++ oli_project/src/pages/Contatos/Contact.jsx
@@ -1,4 +1,7 @@
+//css
 import styles from './styles.module.css';
+
+//images
 import steveSvg from '../../assets/steve.svg';
 
 export default function Contact() {
@@ -8,23 +11,26 @@ export default function Contact() {
       <div className={styles.contactLeft}>
         <img src={steveSvg} />
       </div>
-      <div className={styles.contactRight}>
-        <form className={styles.form}>
-          <div className={styles.formField}>
-            <h2>Nome Completo</h2>
-            <input placeholder='' />
-          </div>
-          <div className={styles.formField}>
-            <h2>E-mail</h2>
-            <input placeholder='' />
-          </div>
-          <div className={styles.formField}>
-            <h2>Mensagem</h2>
-            <textarea placeholder='' />
-          </div>
-          <button>Enviar</button>
-        </form>
+      <div className={styles.contactForm}>
+        <div className={styles.contactRight}>
+          <form className={styles.form}>
+            <div className={styles.formField}>
+              <h2>Nome Completo</h2>
+              <input placeholder='' />
+            </div>
+            <div className={styles.formField}>
+              <h2>E-mail</h2>
+              <input placeholder='' />
+            </div>
+            <div className={styles.formField}>
+              <h2>Mensagem</h2>
+              <textarea placeholder='' />
+            </div>
+            <div className={`btn ${styles.btnEnviar}`}><a href="">Enviar</a></div>
+          </form>
+        </div>
       </div>
+
     </div>
     // </div>
   );
